df0a8a9835c8d8e7996ba8a47c1f0da09885d63b
Bill Graziano <1462628+billgraziano@users.noreply.github.com>
2020-06-14 08:38:14 -0500

    update package and remove patching

--- apply.go
+++ apply.go
@@ -12,7 +12,7 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/inconshreveable/go-update/internal/osext"
+	"github.com/scalesql/go-update/internal/osext"
 )
 
 var (
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/scalesql/go-update
+
+go 1.14
